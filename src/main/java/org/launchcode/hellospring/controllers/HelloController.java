b459ffab02729a03428c26da4e427d657f4fdb4a
FadirTorcer <eric.lyonford@gmail.com>
2022-10-08 01:27:01 -0500

    Made a mess of the exercise.

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -24,8 +24,7 @@ public class HelloController {
     // lives at /hello/hello
     // Handles request of the form /hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    public String helloWithQueryParam(@RequestParam String name, String language) { return createMessage(name, language);
     }
 
     //Handles requests of the form /hello/LaunchCode
@@ -41,10 +40,32 @@ public class HelloController {
                 "<body>" +
                 "<form action='hello' method='post'>" + //submit a request to /hello
                 "<input type='text' name='name'>" +
+                "<select name='language'>" +
+                "<option value='' selected>-What language would you like to be greeted in?-</option>" +
+                "<option value='Spanish'>Spanish</option>" +
+                "<option value='French'>French</option>" +
+                "<option value='Chinese'>Chinese</option>" +
+                "<option value='Norwegian'>Norwegian</option>" +
+                "<option value='English'>English</option>" +
+                "</select>" +
                 "<input type='submit' value='Greet me!'>" +
                 "</form>" +
                 "</body>" +
                 "</html>";
+    }
+
+    public static String createMessage(String name, String language) {
+        if (language == "Spanish") {
+            return "Hola, " + name + "!";
+        } else if (language == "French") {
+            return "Bonjour, " + name + "!";
+        } else if (language == "Chinese") {
+            return "Ni hao, " + name + "!";
+        } else if (language == "Norwegian") {
+            return "Hei, " + name + "!";
+        } else {
+            return "Hello, " + name + "! (" + language + ")";
+        }
     }
 
 }
