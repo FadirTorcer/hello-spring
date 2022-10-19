5e80a5c7c2d2a00d37993f3d97f63c506cfa2d91
FadirTorcer <eric.lyonford@gmail.com>
2022-10-18 21:44:36 -0500

    Reset for starting Ch 13 Thymeleaf

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -3,69 +3,51 @@ package org.launchcode.hellospring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Created by Chris Bay
+ */
 @Controller
-@ResponseBody
-@RequestMapping("hello")
 public class HelloController {
 
-    // Handles request at path /hello
-/*    @GetMapping("hello")
-    @ResponseBody
-    public String hello() {
-        return "Hello, Spring!";
-    }*/
+//    // Handle request at path /hello
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello() {
+//        return "Hello, Spring!";
+//    }
 
-    // lives /hello/goodbye
     @GetMapping("goodbye")
+    @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
     }
 
-    // lives at /hello/hello
-    // Handles request of the form /hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name, String language) { return createMessage(name, language);
+    // Handles requests of the form /hello?name=LaunchCode
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
     }
 
-    //Handles requests of the form /hello/LaunchCode
-    @GetMapping("{name}")
+    // Handles requests of the form /hello/LaunchCode
+    @GetMapping("hello/{name}")
+    @ResponseBody
     public String helloWithPathParam(@PathVariable String name) {
         return "Hello, " + name + "!";
     }
 
-    // lives /hello/form
+
     @GetMapping("form")
+    @ResponseBody
     public String helloForm() {
         return "<html>" +
                 "<body>" +
-                "<form action='hello' method='post'>" + //submit a request to /hello
-                "<input type='text' name='name'>" +
-                "<select name='language'>" +
-                "<option value='' selected>-What language would you like to be greeted in?-</option>" +
-                "<option value='Spanish'>Spanish</option>" +
-                "<option value='French'>French</option>" +
-                "<option value='Chinese'>Chinese</option>" +
-                "<option value='Norwegian'>Norwegian</option>" +
-                "<option value='English'>English</option>" +
-                "</select>" +
-                "<input type='submit' value='Greet me!'>" +
+                "<form action = 'hello' method = 'post'>" + // submit a request to /hello
+                "<input type = 'text' name = 'name' >" +
+                "<input type = 'submit' value = 'Greet Me!' >" +
                 "</form>" +
                 "</body>" +
                 "</html>";
-    }
-
-    public static String createMessage(String name, String language) {
-        if (language == "Spanish") {
-            return "Hola, " + name + "!";
-        } else if (language == "French") {
-            return "Bonjour, " + name + "!";
-        } else if (language == "Chinese") {
-            return "Ni hao, " + name + "!";
-        } else if (language == "Norwegian") {
-            return "Hei, " + name + "!";
-        } else {
-            return "Hello, " + name + "! (" + language + ")";
-        }
     }
 
 }
